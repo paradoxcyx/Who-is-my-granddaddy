bc041357b00b50deb0d9ab45076d1e6732056ad3
Heinrich van Wyk <heinrichvw3410@gmail.com>
2023-12-30 16:00:12 +0200

    - Created data project
    - Added Person entity
    - Added data context
    - Added initial migration to create the person table
    - Defined connection string in app settings

---
+++ WhoIsMyGranddaddy.Data/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WhoIsMyGranddaddy.Data.Models;
+
+public class Person
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public int? FatherId { get; set; }
+    public int? MotherId { get; set; }
+    
+    [ForeignKey("FatherId")]
+    public Person Father { get; set; }
+
+    [ForeignKey("MotherId")]
+    public Person Mother { get; set; }
+    
+    [Required]
+    public string Name { get; set; }
+    
+    [Required]
+    public string Surname { get; set; }
+    
+    [Required]
+    public DateTime BirthDate { get; set; }
+    
+    [Required]
+    public string IdentityNumber { get; set; }
+}
