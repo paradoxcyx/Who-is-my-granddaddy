12ee80f384377cb66eb449a9e745cc7d22188946
Heinrich van Wyk <heinrichvw3410@gmail.com>
2023-12-31 15:07:16 +0200

    Implemented family tree basic service

--- whoismygranddaddy.client/src/app/app.component.ts
+++ whoismygranddaddy.client/src/app/app.component.ts
@@ -1,29 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FamilyTreeApiService} from "./services/family-tree-api.service";
-import {Person} from "./interfaces/person";
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  public persons: Person[] = [];
 
-  constructor(private familyTreeService: FamilyTreeApiService) {}
-
-  ngOnInit() {
-    this.getPeople();
-  }
-
-  getPeople() {
-    this.familyTreeService.getFamilyTree().subscribe(
-      (result) => {
-        this.persons = result.data;
-      },
-      (error) => {
-        console.log(error);
-      }
-    )
-  }
+  ngOnInit(){}
 }
---
+++ whoismygranddaddy.client/src/app/services/service.ts
@@ -0,0 +1,7 @@
+﻿import { HttpClient } from '@angular/common/http';
+
+export class Service {
+  protected baseApiUrl = 'https://localhost:7258';
+
+  constructor(protected httpClient: HttpClient) {}
+}
